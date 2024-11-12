f44aa19e19c38bd49ea6d0f9a6b424f6b9ee0126
ITSamantha <diana.saveleva.dos@gmail.com>
2024-11-12 18:36:51 +0300

    tests added

--- tests/Task7.Tests/UnitTest1.cs
+++ tests/Task7.Tests/UnitTest1.cs
@@ -15,49 +15,49 @@ public class Tests
         public void TestFindNthPrimeInTask7Inf()
         {
             var result = task7inf.findNthPrime(6);
-            Assert.AreEqual(13, result, "The 6th prime should be 13");
+            Assert.That(result, Is.EqualTo(13), "The 6th prime should be 13");
         }
 
         [Test]
         public void TestFindNthPrimeInTask7Module()
         {
             var result = task7module.findNthPrime(6);
-            Assert.AreEqual(13, result, "The 6th prime should be 13");
+            Assert.That(result, Is.EqualTo(13), "The 6th prime should be 13");
         }
 
         [Test]
         public void TestFindNthPrimeInTask7Rec()
         {
             var result = task7rec.findNthPrime(6, 1, 0);
-            Assert.AreEqual(13, result, "The 6th prime should be 13");
+            Assert.That(result, Is.EqualTo(13), "The 6th prime should be 13");
         }
 
         [Test]
         public void TestFindNthPrimeInTask7TailRec()
         {
             var result = task7tailrec.findNthPrime(6);
-            Assert.AreEqual(13, result, "The 6th prime should be 13");
+            Assert.That(result, Is.EqualTo(13), "The 6th prime should be 13");
         }
 
         [Test]
         public void TestFindNthPrimeInTask7Map()
         {
             var result = task7map.findNthPrime(6);
-            Assert.AreEqual(13, result, "The 6th prime should be 13");
+            Assert.That(result, Is.EqualTo(13), "The 6th prime should be 13");
         }
 
         [Test]
         public void TestIsPrimeNumberForPrimeNumber()
         {
             var result = task7module.isPrimeNumber(29);
-            Assert.IsTrue(result, "29 should be prime");
+            Assert.That(result, Is.True, "29 should be prime");
         }
 
         [Test]
         public void TestIsPrimeNumberForNonPrimeNumber()
         {
             var result = task7module.isPrimeNumber(100);
-            Assert.IsFalse(result, "100 should not be prime");
+            Assert.That(result, Is.False, "100 should not be prime");
         }
     }
 }
