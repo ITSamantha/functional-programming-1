a2435cb6c403097954fee6f255b5c7322be0c79e
ITSamantha <diana.saveleva.dos@gmail.com>
2024-11-12 13:13:42 +0300

    feat: moved from gitlab

--- src/task7/map/obj/Release/net8.0/map.AssemblyInfo.fs
+++ src/task7/map/obj/Release/net8.0/map.AssemblyInfo.fs
@@ -7,11 +7,11 @@ open System
 open System.Reflection
 
 
-[<assembly: System.Reflection.AssemblyCompanyAttribute("inf")>]
+[<assembly: System.Reflection.AssemblyCompanyAttribute("map")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Release")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+211eaacad350fd7568ffe6741464fff9f9b91a4a")>]
-[<assembly: System.Reflection.AssemblyProductAttribute("inf")>]
-[<assembly: System.Reflection.AssemblyTitleAttribute("inf")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3084bf8ec2eef1c5db9c26607c50495dc279ce08")>]
+[<assembly: System.Reflection.AssemblyProductAttribute("map")>]
+[<assembly: System.Reflection.AssemblyTitleAttribute("map")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
 do()
